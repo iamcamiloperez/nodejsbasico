53ae306db5731ad5411134da765c3ced8c8baba1
onecris <christhiancamiloperez@gmail.com>
2018-12-01 18:41:13 -0500

    Trabajando con fs.readFile y fs.writeFile y async await

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const fs = require("fs")
 const ops = require("./src/fileops")
 
-let nombres
 
+const leer = (path) => {
+    return new Promise((resolve, reject)=>{
+        fs.readFile(path, 'utf8', (err, lista)=>{
+            if(err) reject(err);
+            resolve(lista.split("\n"))
+        })
+    })
+}
 
-fs.readFile("./resources/name.txt","utf8", (err, lista)=>{
-    if(err) throw err;
-    const nom = lista.split("\n")
-    nombres = ops.callNames(nom)    
-})
+const escribir = (path, valores) => {
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(path, valores, (err, result)=>{
+            if(err) reject(err);
+            resolve(true)            
+        })
+    })
+}
 
-//Escribir nuevo archivo
-fs.writeFile("./resources/namenew.txt", nombres.join("\n"), (err, result)=>{
-    if(err) throw err;
-    console.log("Se escribió el archivo de nombres")
-})
+async function nuevoActualizado(origen , destino){
+    const texto = await leer(origen)
+    const names = ops.callNames(texto).join("\n")
+    await escribir(destino, names)
+    console.log("guardado")        
+}
+
+nuevoActualizado("./resources/name.txt", "./resources/newname.txt")
+
 
 
 /*
