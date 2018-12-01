621c83bdf5a2cecf0cda0f366eb4aa4e0e3f7143
onecris <christhiancamiloperez@gmail.com>
2018-12-01 16:02:00 -0500

    Trabajando con funciones async await

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call")
+
+
+async function callWithPromise(){
+    const fullname = await call.withPromise("Camilo", "Perez")
+        console.log(fullname)
+}
+
+callWithPromise()
+console.log("post prueba")
