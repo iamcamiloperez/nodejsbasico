af87704febf30a87dd308e90e3648686b95ffcce
onecris <christhiancamiloperez@gmail.com>
2018-12-01 15:45:59 -0500

    trabajando con Callbacs y Promises

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+const linea = () => console.log("==============================")
+
+console.log(call.sync("Cris"));
+linea();
+
+call.withCallback("cris", call.sync)
+linea();
+
+
+call.withPromise("Camilo")
+    .then(name=>console.log(name))
