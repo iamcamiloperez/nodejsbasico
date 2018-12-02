a461be271a559cadea709af1b1f5ed76954f96dc
onecris <christhiancamiloperez@gmail.com>
2018-12-02 10:33:37 -0500

    Creando un servidor local con http

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require("http")
+const fs = require("fs")
+
+const server = http.createServer((req, res) => {
+    fs.readFile("./resources/my_page.html",(err, data)=>{
+        if(err){console.error; return;}
+        res.end(data)
+    })
+})
+
+console.log("Iniciando servidor...")
+server.listen(3030)
