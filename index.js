487f61ca2b1dd4348237883b5ac96d4b39ec19a4
onecris <christhiancamiloperez@gmail.com>
2018-12-02 11:01:02 -0500

    Creando servidor local con express

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.send("Home path")
+})
+
+app.get("/app", (req, res)=>{
+    res.send("Home/app path")
+})
+
+app.get("*", (req, res)=>{
+    res.send("No se donde estoy")
+})
+
+app.listen(3030, ()=>{
+    console.log("Servidor express escuchando en el puerto 3030")
+})
