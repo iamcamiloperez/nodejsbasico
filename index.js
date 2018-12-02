cf8cbfe9ce010c4e251dbf9e0bd3a8ad38ca65ef
onecris <christhiancamiloperez@gmail.com>
2018-12-01 22:46:05 -0500

    Trabajando con childProcess para ejecutar comandos de consola desde node

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require("child_process")
+
+function comando(command){
+    cp.exec(command, (err, stdout, stderr)=>{
+        if(err){
+            console.log(`Error: ${err}`)
+            return
+        }
+        if(stdout) console.log(`Standard out: \n ${stdout}`)
+        if(stderr) console.log(`Standard err: \n ${stderr}`)
+    })
+}
+
+//comando("ls");
+comando("npm install")
+//comando("node ./src/script --base=5")
