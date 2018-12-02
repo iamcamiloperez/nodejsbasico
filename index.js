7c8b5f9ba7a80747e1f541c2fa01bae707d03d15
onecris <christhiancamiloperez@gmail.com>
2018-12-01 19:12:22 -0500

    Trabajando con readline

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const q = [
+    "Nombre: ",
+    "Apellido: ",
+    "Edad: "
+]
+
+const askQuestion = (rl, question) => {
+    return new Promise((resolve, rej)=>{
+        rl.question(question, answer=>{
+            resolve(answer)
+        })
+    })
+}
+
+const Ask = function(questions){
+    return new Promise(async resolve => {
+        let results = [];
+        for (let i = 0; i < questions.length; i++) {
+            const result = await askQuestion(rl, questions[i])            
+            result.push(result);
+        }
+        rl.close();
+        resolve(results)
+    })
+}
+
+
+Ask(q)
+    .then(q => {
+        console.log(`Nombre: ${q[0]} ${q[1]}`)
+    });
